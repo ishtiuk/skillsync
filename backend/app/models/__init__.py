e00010a94beac68a2c38ec6e45edb9407c84430b
ishtiuk <ishtigit@gmail.com>
2025-06-03 19:50:22 +0600

    models

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,8 @@
+from .cover_letters import CoverLetter
+from .experience import Experience
+from .milestone import Milestone
+from .organization import Organization
+from .portfolio import Portfolio
+from .position import Position
+from .user import UserCareerForge, UserTalentHub, BaseUser, UserFiles
+from .job_application import JobApplication
