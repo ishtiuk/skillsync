21cd806145924c06e8d1d4556ed1d6c4c9d59217
ishtiuk <ishtigit@gmail.com>
2025-06-05 01:56:16 +0600

    init

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,2 +1,9 @@
+from .user import BaseUser, UserCareerforge, UserTalenthub, UserFiles
+from .tracked_jobs import TrackedJobs
+from .cover_letters import CoverLetters
 # from .feature_tracking import FeatureTracking
+from .experience import Experiences
+from .positions import Positions
 # from .payments import Payments
+from .milestones import Milestones
+from .organization import Organizations
